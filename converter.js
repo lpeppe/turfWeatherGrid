4a4bd9f5558b7d0241cd4a22286fba2453cfade2
Luca Peppe <luca@Lucas-MacBook-Pro.local>
2017-11-28 14:53:12 +0100

    major bugfix

--- converter.js
+++ converter.js
@@ -1,70 +1,79 @@
 const turfGrid = require('@turf/square-grid');
+const { promisify } = require('util');
 const turfHelpers = require('@turf/helpers');
 const turfMeta = require('@turf/meta');
+const turfUnion = require('@turf/union');
+const turfCenter = require('@turf/center-of-mass');
 const fs = require('fs');
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 async function init() {
     var polygons;
+    var unionGrid;
     try {
-        polygons = await loadGrid();
+        polygons = JSON.parse(await (readFile('grid.json')))
         console.log("Grid loaded!")
     }
     catch (err) {
         console.log("No grid found!");
         console.log("Computing a new grid...");
-        await createGrid();
-        polygons = await loadGrid();
+        var mask = JSON.parse(await (readFile('italy.json')))
+        var polygons = createGrid(mask);
+        await writeFile('grid.json', JSON.stringify(polygons));
     }
     turfMeta.featureEach(polygons, (currentFeature, featureIndex) => {
-        if (Math.random() >= 0.3)
+        if (Math.random() < 0.1)
             currentFeature.properties.sunny = true
+        else
+            currentFeature.properties.sunny = false
+
     })
-    await saveGrid("weatherGrid", polygons);
+    await writeFile('weatherGrid.json', JSON.stringify(polygons));
     console.log("Weather grid saved!")
+    // try {
+    //     unionGrid = await loadGrid("unionGrid")
+    //     console.log("Union Grid loaded!")
+    // }
+    // catch (err) {
+    //     console.log("No grid found!");
+    //     console.log("Computing a new grid...");
+    //     unionGrid = await saveGrid("unionGrid", computeUnion(polygons));
+    //     console.log("Union grid saved!")
+    // }
+    console.log("Computing points...")
+    await writeFile('centerPoints.json', JSON.stringify(calculatePoints(polygons)));
+    console.log("Points computed!")
 }
 
-
-function loadGrid() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('grid.json', 'utf8', (err, data) => {
-            if (err)
-                reject(err)
-            else
-                resolve(JSON.parse(data))
-        })
-    })
+function createGrid(mask) {
+    var poly1 = turfHelpers.polygon(mask.features[0].geometry.coordinates)
+    var bbox = [4.6582031, 36.0668621, 21.9287109, 49.1529697];
+    var cellSide = 10;
+    var options = {
+        units: 'kilometers',
+        mask: poly1
+    };
+    return turfGrid(bbox, cellSide, options)
 }
 
-function createGrid() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('italy.json', 'utf8', (err, data) => {
-            if (err)
-                reject(err)
-            else
-                resolve(JSON.parse(data))
-        })
+function computeUnion(weatherGrid) {
+    var sunnyPoligons = [];
+    turfMeta.featureEach(weatherGrid, (currentFeature, featureIndex) => {
+        if (currentFeature.properties.sunny)
+            sunnyPoligons.push(currentFeature)
     })
-        .then(data => {
-            var poly1 = turfHelpers.polygon(data.features[0].geometry.coordinates)
-            var bbox = [4.6582031, 36.0668621, 21.9287109, 49.1529697];
-            var cellSide = 5;
-            var options = {
-                units: 'kilometers',
-                mask: poly1
-            };
-            var squareGrid = turfGrid(bbox, cellSide, options)
-            return saveGrid("grid", squareGrid);
-        })
+    return turfUnion(...sunnyPoligons);
 }
 
-function saveGrid(fileName, grid) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(fileName + ".json", JSON.stringify(grid), err => {
-            if (err)
-                reject(err)
-            resolve();
-        })
+function calculatePoints(grid) {
+    var points = [];
+    turfMeta.flattenEach(grid, (currentFeature, featureIndex, featureSubIndex) => {
+        let point = turfCenter(currentFeature);
+        point.properties.sunny = currentFeature.properties.sunny;
+        points.push(point)
     })
+    return turfHelpers.featureCollection(points);
 }
 
 init();
